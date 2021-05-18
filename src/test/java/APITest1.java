9684d7b213b891823d0417b932faf1cc97432f6f
AlexeyVFrolov <isbase@mail.ru>
2021-05-18 08:14:44 +0300

    Initial commit

---
+++ src/test/java/APITest1.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class APITest1 {
+
+    @Test
+    void shouldReturnPostedBody2() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+        .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+        .body("data", equalTo("some data"))
+        ;
+    }
+}
